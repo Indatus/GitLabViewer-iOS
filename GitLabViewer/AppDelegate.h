952bf9896bccc16d9601b88e800582a2c517ac7e
Jon Staff <jstaff@indatus.com>
2014-02-04 17:19:13 -0500

    Created Xcode project.

---
+++ GitLabViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitLabViewer
+//
+//  Created by Jon Staff on 2/4/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
