d2ff5988789092762e5e0dea77f9168473a716a1
Jon Staff <jstaff@indatus.com>
2014-02-04 17:23:28 -0500

    Added the LoginViewController

---
+++ GitLabViewer/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  GitLabViewer
+//
+//  Created by Jon Staff on 2/4/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
