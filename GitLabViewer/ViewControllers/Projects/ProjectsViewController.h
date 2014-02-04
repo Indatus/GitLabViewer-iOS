eb9b1905623e65e73d8a62002aaa8716f27aebe3
Jon Staff <jstaff@indatus.com>
2014-02-04 17:43:04 -0500

    Added empty ViewControllers for Home and Projects.

---
+++ GitLabViewer/ViewControllers/Projects/ProjectsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectsViewController.h
+//  GitLabViewer
+//
+//  Created by Jon Staff on 2/4/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectsViewController : UITableViewController
+
+@end
