a6a633fa19912bc4a9c35f1dd8808306799d5005
Jon Staff <jstaff@indatus.com>
2014-02-11 18:41:25 -0500

    Cancel gestures and keyboard methods added to the create project page

--- GitLabViewer/ViewControllers/Projects/CreateProjectViewController.m
+++ GitLabViewer/ViewControllers/Projects/CreateProjectViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "CreateProjectViewController.h"
+#import "UIAlertView+Indatus.h"
 
 @interface CreateProjectViewController ()
+{
+    UIGestureRecognizer *_cancelKeyboardGesture;
+}
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UITextField *inputTitle;
 @property (weak, nonatomic) IBOutlet UITextField *inputDescription;
 @property (weak, nonatomic) IBOutlet UISwitch *switchIssues;
@@ -40,6 +45,8 @@
     
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelModal)];
     self.navigationItem.leftBarButtonItem = cancelButton;
+    
+    [self addKeyboardObservers];
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,7 +60,19 @@
 
 - (IBAction)btnCreateClicked:(id)sender
 {
+    NSString *title = _inputTitle.text;
     
+    if (title.length < 1) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"You must specify at least a title." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Create Project?" message:@"Please verify that all the information is correct." delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:@"Create", nil];
+        [alert showWithCompletion:^(UIAlertView *alertView, NSInteger buttonIndex) {
+            if (buttonIndex != [alertView cancelButtonIndex]) {
+                
+            }
+        }];
+    }
 }
 
 
@@ -63,5 +82,49 @@
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
+#pragma mark - Keyboard methods
+
+- (void)addKeyboardObservers
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:self.view.window];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:self.view.window];
+}
+
+- (void)keyboardDidShow:(NSNotification *)notification
+{
+    _cancelKeyboardGesture = [UITapGestureRecognizer new];
+    [_cancelKeyboardGesture addTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:_cancelKeyboardGesture];
+    
+    CGRect viewFrame = _scrollView.frame;
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    viewFrame.size.height -= keyboardSize.height;
+    _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width, _scrollView.frame.size.height - 100); // the '100' is just an arbitrary value that seems to work well
+    _scrollView.frame = viewFrame;
+}
+
+- (void)keyboardDidHide:(NSNotification *)notification
+{
+    CGRect viewFrame = _scrollView.frame;
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    viewFrame.size.height += keyboardSize.height;
+    _scrollView.frame = viewFrame;
+    _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width, _scrollView.frame.size.height - 100); // the '100' is just an arbitrary value that seems to work well
+}
+
+- (void)dismissKeyboard
+{
+    [self.view endEditing:YES];
+    
+    if(_cancelKeyboardGesture) {
+        [self.view removeGestureRecognizer:_cancelKeyboardGesture];
+        _cancelKeyboardGesture = nil;
+    }
+}
+
 
 @end
