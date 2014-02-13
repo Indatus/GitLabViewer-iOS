44960d7d95f783943b6ebd3c958c1bc783f5740f
Jon Staff <jstaff@indatus.com>
2014-02-13 14:37:43 -0500

    Added new files for creating MergeRequests

---
+++ GitLabViewer/ViewControllers/Merge Requests/CreateMergeRequestViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreateMergeRequestViewController.m
+//  GitLabViewer
+//
+//  Created by Jon Staff on 2/13/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import "CreateMergeRequestViewController.h"
+
+@interface CreateMergeRequestViewController ()
+
+@end
+
+@implementation CreateMergeRequestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
