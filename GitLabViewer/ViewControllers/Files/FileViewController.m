1da18218bf755fb13c75c26739a358aa00a33375
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2014-02-25 13:03:16 -0500

    text files load their contents for display

--- GitLabViewer/ViewControllers/Files/FileViewController.m
+++ GitLabViewer/ViewControllers/Files/FileViewController.m
@@ -10,22 +10,67 @@
 #import <GLFile.h>
 #import <GLGitlabApi+Files.h>
 
-@interface FileViewController ()
+@interface FileViewController () <UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
 @implementation FileViewController
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self fetchData];
+}
 
 - (void)fetchData
 {
     [[GLGitlabApi sharedInstance] getFileContentFromProject:self.project.projectId
                                                         sha:_file.fileId
                                            withSuccessBlock:^(id responseObject) {
-                                               NSLog(@"Response Object: %@", responseObject);
+                                               [self showTextResponse:responseObject];
+                                               [_activityIndicator stopAnimating];
                                            }
                                             andFailureBlock:^(NSError *error) {
                                                 NSLog(@"Failure: %@", error);
+                                                [self showFailureMessage];
+                                                [_activityIndicator stopAnimating];
                                             } ];
+}
+
+- (void)showTextResponse:(NSData *)data
+{
+    UITextView *textView = [[UITextView alloc] initWithFrame:self.view.bounds];
+    textView.editable = NO;
+    textView.text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    textView.alpha = 0;
+    [self.view addSubview:textView];
+    
+    [UIView animateWithDuration:.25
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         textView.alpha = 1;
+                     }
+                     completion:nil];
+}
+
+- (void)showFailureMessage
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                    message:@"The file could not be downloaded, try again."
+                                                   delegate:self
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:@"OK", nil];
+    [alert show];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
